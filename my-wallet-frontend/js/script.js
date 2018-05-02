8954ccc8b3602250d714e9a01f0eb52f36dd53ea
mslowiak <marcin.slowiak.007@gmail.com>
2018-05-02 16:37:56 +0200

    Added sample history page

--- my-wallet-frontend/js/script.js
+++ my-wallet-frontend/js/script.js
@@ -42,25 +42,42 @@ webWalletApp.controller('historyController', function($scope) {
     $scope.message = 'History';
 });
 
-webWalletApp.controller('listOfCategoriesController', function($scope, $http, $log) {
+webWalletApp.controller('listOfCategoriesController', function($scope, $http) {
+    $scope.message = 'List of categories';
+
     $scope.loadData = $http.get('http://localhost:8080/api/categories').success(function (response) {
         $scope.categories = response;
     });
+
     $scope.sortType = 'name';
     $scope.sortReverse = false;
-
-    $scope.TestClick = function(){
-        $log.log($scope.sortType);
-        $log.log($scope.sortReverse);
-    };
-
-    $scope.message = 'List of categories';
 });
-webWalletApp.controller('listOfExpensesController',  function($scope) {
+webWalletApp.controller('listOfExpensesController',  function($scope, $http) {
     $scope.message = 'List of expenses';
+    $scope.loadData = $http.get('http://localhost:8080/api/categories').success(function (response) {
+        $scope.expenses = response;
+    });
 });
 webWalletApp.controller('summaryController', function($scope) {
     $scope.message = 'Summary';
+
+    new Chart(document.getElementById("pie-chart"), {
+        type: 'pie',
+        data: {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [{
+                label: "Population (millions)",
+                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                data: [2478,5267,734,784,433]
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
+            }
+        }
+    });
 });
 webWalletApp.controller('importDataController', function($scope) {
     $scope.message = 'Import Data';
