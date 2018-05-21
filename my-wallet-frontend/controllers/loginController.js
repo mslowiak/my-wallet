b18c895f22528d00589f6d363f0ee4b95420e1a6
mslowiak <marcin.slowiak.007@gmail.com>
2018-05-21 21:24:42 +0200

    Login process with error handling

--- my-wallet-frontend/controllers/loginController.js
+++ my-wallet-frontend/controllers/loginController.js
@@ -2,16 +2,31 @@ webWalletApp.controller('loginController', function ($rootScope, $scope, $http) 
     document.getElementById("sidebar").style.display = 'none';
     webWalletApp.value('loggedIn', true);
     document.getElementById('login-button').addEventListener("click", function () {
-        // post request
+        var login = document.getElementById('username-input').value;
+        var password = CryptoJS.SHA256(document.getElementById('password-input').value).toString();
+        var errorLabel = document.getElementById('error-login-label');
+        var post = $http({
+            method: "POST",
+            url: "http://localhost:8080/api/login",
+            data: {login: login, password: password}
+        }).then(function (response) {
+            $rootScope.isLogged = true;
+            $rootScope.loggedUserId = response.data.userId;
+            errorLabel
+        }).catch(function (reason) {
+            errorLabel.innerHTML = reason.data.result;
+            errorLabel.style.display = 'block';
+        });
     });
     document.getElementById('register-button').addEventListener("click", function () {
-        console.log("XDD");
         document.getElementById('sign-up-form').style.display = 'none';
         document.getElementById('register-form').style.display = 'block';
+        document.getElementById('error-login-label').style.display = 'none';
     });
     document.getElementById('sign-up-button').addEventListener("click", function () {
         document.getElementById('sign-up-form').style.display = 'block';
         document.getElementById('register-form').style.display = 'none';
+        document.getElementById('error-login-label').style.display = 'none';
     });
     document.getElementById('register-now-button').addEventListener("click", function () {
         // post request
