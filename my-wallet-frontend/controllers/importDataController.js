79a8e5faa445125439c1ec173db66ae2fe3996d5
mslowiak <marcin.slowiak.007@gmail.com>
2018-05-24 23:10:32 +0200

    File uploading

--- my-wallet-frontend/controllers/importDataController.js
+++ my-wallet-frontend/controllers/importDataController.js
@@ -1,3 +1,29 @@
-webWalletApp.controller('importDataController', function ($scope) {
+webWalletApp.controller('importDataController', function ($scope, $http) {
     $scope.message = 'Import Data';
+
+    document.getElementById('upload-button').addEventListener("click", function () {
+        console.log("trying");
+        var filePath = document.getElementById('file-input').files[0];
+        console.log(filePath);
+        var formdata = new FormData();
+        formdata.append("file", filePath);
+        var post = $http({
+            method: "POST",
+            url: "http://localhost:8080/api/transactions/upload",
+            enctype: 'multipart/form-data',
+            data: formdata,
+            processData: false,
+            contentType: false,
+            headers : {
+                'Content-Type' : undefined
+            }
+        }).then(function (response) {
+            document.getElementById('success-import-file').style.display = 'block';
+            document.getElementById('error-import-file').style.display = 'none';
+        }, function (error) {
+            console.log("catch");
+            document.getElementById('success-import-file').style.display = 'none';
+            document.getElementById('error-import-file').style.display = 'block';
+        });
+    });
 });
